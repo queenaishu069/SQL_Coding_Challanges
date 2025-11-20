e932310ea2ce2932cb9f88736e4014c1e2ef0778
Iswarya G <queenaishu069@gmail.com>
2025-11-20 03:49:26 -0800

    Refactor comments in SQL script for better readability
    Updated comments for clarity and consistency in SQL script.

--- Coding Challange - DAY 4.sql
+++ Coding Challange - DAY 4.sql
@@ -1,15 +1,15 @@
--- 1️.Create Database
+-- Create Database
 CREATE DATABASE ECommerceDB;
 USE ECommerceDB;
 
--- 2️.Create Product Table
+-- Create Product Table
 CREATE TABLE Product (
     product_id INT PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
     price DECIMAL(10,2) CHECK (price > 0)
 );
 
--- 3️.Create Sales Table
+-- Create Sales Table
 CREATE TABLE Sales (
     sale_id INT PRIMARY KEY,
     product_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE Sales (
     FOREIGN KEY (product_id) REFERENCES Product(product_id)
 );
 
--- 4️.Insert Sample Data into Product Table
+-- Insert Sample Data into Product Table
 INSERT INTO Product (product_id, product_name, price) VALUES
 (1, 'Laptop', 85000.00),
 (2, 'Smartphone', 45000.00),
@@ -28,7 +28,7 @@ INSERT INTO Product (product_id, product_name, price) VALUES
 (6, 'Monitor', 15000.00),
 (7, 'Webcam', 3500.00);
 
--- 5️.Insert Sample Data into Sales Table
+-- Insert Sample Data into Sales Table
 INSERT INTO Sales (sale_id, product_id, quantity, sale_amount) VALUES
 (1, 1, 2, 170000.00),
 (2, 2, 3, 135000.00),
@@ -80,6 +80,7 @@ JOIN Sales s
 ON p.product_id = s.product_id
 GROUP BY p.product_name,s.product_id
 ORDER BY SUM(s.sale_amount) DESC;
+
 -- Limit  with group by 
 SELECT p.product_name,
 s.product_id, SUM(s.sale_amount) total_amount
